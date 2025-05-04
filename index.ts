df858f7f1bec6c0cb263dbe8e7370a5fa2e4f595
nando-z <157473087+nando-z@users.noreply.github.com>
2025-05-04 19:55:22 +0300

    fix(Core): :zap: the core seting of the serve

--- index.ts
+++ index.ts
@@ -2,13 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import router from './core/routes/route';
 const app = express();
+const env = dotenv.config();
+if(process.env.NODE_ENV  !== 'production'){
+    env;
+}
 
 const PORT = process.env.PORT || 8000
 const HOST = process.env.HOST || "localhost";
 app.use('/', router);
 
 // Server Run
-app.listen(PORT ,() => {
-    console.dir(`Server Run in http://{HOST}:${PORT}`);
-});
+app.listen(PORT,
+    ()=>console.dir(`:: Server Start at https://${HOST}:${PORT}`)
+);
 
